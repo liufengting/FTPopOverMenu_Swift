58ad5c0cab2adf7d44e2fe5b65489cfb56ae0dbc
Abdullah Selek <abdullahselek@gmail.com>
2017-07-28 21:02:44 +0200

    create new separate class for UIControl extension

--- FTPopOverMenu_Swift/FTPopOverMenu.swift
+++ FTPopOverMenu_Swift/FTPopOverMenu.swift
@@ -263,29 +263,6 @@ public class FTPopOverMenu : NSObject {
     }
 
 }
-extension UIControl {
-    
-    // solution found at: http://stackoverflow.com/a/5666430/6310268
-    
-    fileprivate func setAnchorPoint(anchorPoint: CGPoint) {
-        var newPoint = CGPoint(x: self.bounds.size.width * anchorPoint.x, y: self.bounds.size.height * anchorPoint.y)
-        var oldPoint = CGPoint(x: self.bounds.size.width * self.layer.anchorPoint.x, y: self.bounds.size.height * self.layer.anchorPoint.y)
-        
-        newPoint = newPoint.applying(self.transform)
-        oldPoint = oldPoint.applying(self.transform)
-        
-        var position = self.layer.position
-        position.x -= oldPoint.x
-        position.x += newPoint.x
-        
-        position.y -= oldPoint.y
-        position.y += newPoint.y
-        
-        self.layer.position = position
-        self.layer.anchorPoint = anchorPoint
-    }
-    
-}
 
 extension FTPopOverMenu {
     
---
+++ FTPopOverMenu_Swift/UIControl+Point.swift
@@ -0,0 +1,33 @@
+//
+//  UIControl+Point.swift
+//  FTPopOverMenu_Swift
+//
+//  Created by Abdullah Selek on 28/07/2017.
+//  Copyright © 2017 LiuFengting. All rights reserved.
+//
+
+import UIKit
+
+extension UIControl {
+
+    // solution found at: http://stackoverflow.com/a/5666430/6310268
+
+    internal func setAnchorPoint(anchorPoint: CGPoint) {
+        var newPoint = CGPoint(x: self.bounds.size.width * anchorPoint.x, y: self.bounds.size.height * anchorPoint.y)
+        var oldPoint = CGPoint(x: self.bounds.size.width * self.layer.anchorPoint.x, y: self.bounds.size.height * self.layer.anchorPoint.y)
+
+        newPoint = newPoint.applying(self.transform)
+        oldPoint = oldPoint.applying(self.transform)
+
+        var position = self.layer.position
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+
+        self.layer.position = position
+        self.layer.anchorPoint = anchorPoint
+    }
+
+}
